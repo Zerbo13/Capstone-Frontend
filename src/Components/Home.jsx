9adb84808a3b4fe624bb5469d5f6e2be535a79b3
Mattia Zerbini <zermatty6@gmail.com>
2026-03-05 11:27:17 +0100

    Create react project

---
+++ src/Components/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home(){
+    return(
+    <div className="container mt-5">
+        <h1>Benvenuto nel nostro sito di padel!!</h1>
+        <p>Hai effettuato correttamente l'accesso.</p>
+    </div>
+    );
+}
